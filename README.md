a2aaf73dc5d39dacd69ff87b86913d338521992e
dymafr <erwan1309@hotmail.com>
2022-04-01 15:12:56 +0200

    2 files modified, 1 added

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# vitejs-vite-keewph
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-keewph)
+Formation Vue Dyma
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import { routes } from './routes';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
 
 const app = createApp(App);
 
