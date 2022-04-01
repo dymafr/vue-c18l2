1655f5b56bb198f662f41811e9f939cf00edb343
dymafr <erwan1309@hotmail.com>
2022-04-01 15:15:04 +0200

    rm

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,6 @@
 </template>
 
 <script setup lang="ts">
-import PageA from './PageA.vue';
-import PageB from './PageB.vue';
-import { ref, type Component as C } from 'vue';
-
-const composants: { [s:string]: C } = {
-  PageA,
-  PageB,
-};
-
-const selectedComponant = ref('PageA');
 </script>
 
 <style lang="scss">
